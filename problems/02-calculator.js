80a97faa11e8e59a51c48bc5fe5fc1927f1d253c
Connie Ooi <connie.ooi18@gmail.com>
2023-02-15 14:00:20 -0800

    02 done

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,30 @@
 // Your code here
 
+class Calculator {
+	total = 0
+	add(num){
+		this.total += num;
+		return this.total;
+	}
+
+	subtract(num){
+		this.total -= num;
+		return this.total;
+	}
+	multiply(num){
+		this.total *= num;
+		return this.total;
+	}
+	divide(num){
+		this.total /= num;
+		return this.total;
+	}
+}
+
+
+// let calculator = new Calculator();
+// console.log(calculator.subtract(35))
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
